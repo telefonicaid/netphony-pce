55670d86f4be0f636416d8c7b878c80969044402
Alejandro <b.aam@tid.es>
2014-06-06 13:51:38 +0200

    Cleaning LSAs

--- src/tid/pce/server/RedisTEDUpdaterThread.java
+++ src/tid/pce/server/RedisTEDUpdaterThread.java
@@ -4,13 +4,10 @@ package tid.pce.server;
 import java.util.LinkedList;
 import java.util.concurrent.LinkedBlockingQueue;
 
-import org.eclipse.jetty.util.log.Log;
-
 import tid.ospf.ospfv2.OSPFv2LinkStateUpdatePacket;
 import tid.ospf.ospfv2.lsa.LSA;
 import tid.ospf.ospfv2.lsa.LSATypes;
 import tid.ospf.ospfv2.lsa.OSPFTEv2LSA;
-
 import tid.pce.tedb.DatabaseControlSimplifiedLSA;
 
 public class RedisTEDUpdaterThread extends Thread{
@@ -39,7 +36,8 @@ public class RedisTEDUpdaterThread extends Thread{
 				for (int i =0;i< lsaList.size();i++){
 					if (lsaList.get(i).getLStype() == LSATypes.TYPE_10_OPAQUE_LSA){
 						lsa=(OSPFTEv2LSA)lsaList.get(i);
-						DatabaseControlSimplifiedLSA dcsl = lsa.getSimplifiedLsa();
+						DatabaseControlSimplifiedLSA dcsl = new DatabaseControlSimplifiedLSA();
+						dcsl.fillSimplifiedLsa(lsa);
 						String jsonLSA = dcsl.logJsonSimplifiedLSA();
 						RedisDatabaseHandler rdh = new RedisDatabaseHandler();
 						rdh.write("LSA:"+dcsl.getAdvertisingRouter().getHostAddress()+":"+dcsl.getLinkId().getHostAddress(),jsonLSA);
