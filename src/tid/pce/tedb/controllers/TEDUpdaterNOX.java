dcf4899b0c72b9ab149a35a9f74c2020fd3f5fc1
Alejandro <b.aam@tid.es>
2014-06-11 10:56:09 +0200

    TEDUpdaterNOX is working now.

--- src/tid/pce/tedb/controllers/TEDUpdaterNOX.java
+++ src/tid/pce/tedb/controllers/TEDUpdaterNOX.java
@@ -7,6 +7,7 @@ import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.concurrent.locks.Lock;
 import java.util.logging.Logger;
 
@@ -69,25 +70,11 @@ public class TEDUpdaterNOX extends TEDUpdaterController
 			
 			for (int i = 0; i < ips.size(); i++)
 			{
-				responseNodes = queryForNodes(ips.get(i), ports.get(i));
+				responseNodes = queryForNodes(ips.get(i), ports.get(i));//query for topology
 				parseNodes(responseNodes, nodes, ips.get(i), ports.get(i));
 				log.info("responseNodes:::"+responseNodes);
 			}
 			
-			for (int i = 0; i < ips.size(); i++)
-			{			
-				//responseLinks = queryForLinks(ips.get(i), ports.get(i));	
-						
-		        //log.info("responseLinks:::"+responseLinks);
-		        
-		        //lock();
-		        //parseLinks(responseLinks, nodes);
-		        //unlock();
-	        
-			}
-	        			
-	        //parseJSON("[{\"src-switch\":\"00:14:2c:59:e5:5e:2b:00\",\"src-port\":20,\"src-port-state\":0,\"dst-switch\":\"00:14:2c:59:e5:64:21:00\",\"dst-port\":19,\"dst-port-state\":0,\"type\":\"internal\"},{\"src-switch\":\"00:14:2c:59:e5:64:21:00\",\"src-port\":19,\"src-port-state\":0,\"dst-switch\":\"00:14:2c:59:e5:5e:2b:00\",\"dst-port\":20,\"dst-port-state\":0,\"type\":\"internal\"},{\"src-switch\":\"00:14:2c:59:e5:66:ed:00\",\"src-port\":9,\"src-port-state\":0,\"dst-switch\":\"00:14:2c:59:e5:64:21:00\",\"dst-port\":20,\"dst-port-state\":0,\"type\":\"internal\"},{\"src-switch\":\"00:14:2c:59:e5:64:21:00\",\"src-port\":20,\"src-port-state\":0,\"dst-switch\":\"00:14:2c:59:e5:66:ed:00\",\"dst-port\":9,\"dst-port-state\":0,\"type\":\"internal\"}]");
-	        //System.out.println(response);
 		}
 		catch (Exception e)
 		{
@@ -116,30 +103,32 @@ public class TEDUpdaterNOX extends TEDUpdaterController
 			Object obj = parser.parse(response);
 		
 			JSONObject jsonaux=(JSONObject) obj;
-			JSONArray msg=(JSONArray) jsonaux.get("switches");
-
-			
-			Iterator<JSONObject> iterator = msg.iterator();
-			while (iterator.hasNext()) 
-			{	
-				JSONObject jsonObject = (JSONObject) iterator.next();
-				
-				log.info("(String)jsonObject.get(dpid)::"+(String)jsonObject.get("dpid"));
-				
-				RouterInfoPM rInfo = new RouterInfoPM();
-				
-				rInfo.setRouterID((String)jsonObject.get("dpid"));//Bristol to floodlight?
-				rInfo.setConfigurationMode("Openflow");
-				rInfo.setControllerType(TEDUpdaterNOX.controllerName);
-				
-				rInfo.setControllerIdentifier(ip, port);
-				rInfo.setControllerIP(ip);
-				rInfo.setControllerPort(port);
-				
-				routerInfoList.put(rInfo.getRouterID(),rInfo);
-								
-				((SimpleTEDB)TEDB).getNetworkGraph().addVertex(rInfo);
+			Iterator<String> iterkeys=jsonaux.keySet().iterator();
+			Hashtable<String,RouterInfoPM> nodes= new Hashtable<String,RouterInfoPM>();
+			while (iterkeys.hasNext()) {
+					String dpid = iterkeys.next();
+					System.out.println("Key: "+dpid.replace("-", ":"));
+					RouterInfoPM rInfo = new RouterInfoPM();
+					
+					rInfo.setRouterID(dpid.replace("-", ":"));//Bristol to floodlight?
+					rInfo.setConfigurationMode("Openflow");
+					rInfo.setControllerType(TEDUpdaterNOX.controllerName);
+					
+					rInfo.setControllerIdentifier(ip, port);
+					rInfo.setControllerIP(ip);
+					rInfo.setControllerPort(port);
+					
+					routerInfoList.put(rInfo.getRouterID(),rInfo);
+					nodes.put(dpid.replace("-", ":"),rInfo);				
+					((SimpleTEDB)TEDB).getNetworkGraph().addVertex(rInfo);
 			}
+			//Getting Links
+			Iterator<String> nodeiter=nodes.keySet().iterator();
+			while (nodeiter.hasNext()){
+				String node= nodeiter.next();
+				parseLinks(node, (String)jsonaux.get(node.replace(":", "-")).toString(), nodes);
+			}
+			//parseLinks(dpid, jsonarray)
 		}
 		catch (Exception e)
 		{
@@ -147,7 +136,7 @@ public class TEDUpdaterNOX extends TEDUpdaterController
 		}
 	}
 	
-	private void parseLinks(String links,Hashtable<String,RouterInfoPM> nodes)
+	private void parseLinks(String dpid,String links, Hashtable<String,RouterInfoPM> nodes)
 	{
 		try {
 			//log.info("Inside parseJSON");
@@ -162,75 +151,55 @@ public class TEDUpdaterNOX extends TEDUpdaterController
 				//System.out.println(jsonObject.get("src-switch"));
 				IntraDomainEdge edge= new IntraDomainEdge();
 				
-				JSONObject jsonObject_src = (JSONObject) jsonObject.get("src");
-				JSONObject jsonObject_dst = (JSONObject) jsonObject.get("dst");
+				String labels = (String) jsonObject.get("labels");//??
+				String destnode = ((String) jsonObject.get("peer_node")).replace("-", ":");
+				String type = (String) jsonObject.get("port_type");
 
-				RouterInfoPM source = nodes.get(BristoltoFloodlight((String)jsonObject_src.get("dpid")));
-				RouterInfoPM dest = nodes.get(BristoltoFloodlight((String)jsonObject_dst.get("dpid")));
+				RouterInfoPM source = nodes.get(dpid);
+				RouterInfoPM dest = nodes.get(destnode);
 					
 				//((SimpleTEDB)TEDB).getNetworkGraph().addVertex(source);
 				//((SimpleTEDB)TEDB).getNetworkGraph().addVertex(dest);
-
-				log.info("Adding Vertex->"+source+" hashcode:"+source.hashCode());
-				log.info("Adding Vertex->"+dest+" hashcode:"+dest.hashCode());
 				
-				edge.setSrc_if_id((Long)jsonObject_src.get("port_no"));
-				edge.setDst_if_id((Long)jsonObject_dst.get("port_no"));
+				System.out.println("Srcnode: "+dpid);
+				System.out.println("Dstnode: "+destnode);
 				
+				System.out.println("Srcport: "+jsonObject.get("port_id"));
+				System.out.println("Dstport: "+jsonObject.get("peer_port"));
 				
-				// This is a big problem because info is not initialized from file
-				// and the controller doesn't give information about how many wlans
-				// the are
-				
-				TE_Information tE_info = new TE_Information();
-				tE_info.setNumberWLANs(15);
-				tE_info.initWLANs();
-				
-				if (interDomainFile != null)
-				{
-					completeTE_Information(tE_info, source.getRouterID(), dest.getRouterID());
-				}
-				
-				edge.setTE_info(tE_info);
-				
-				String isBidirectional = (String)jsonObject.get("direction");
-				
-				
-				// En Bristol los enlaces son unidirecctionales, pero asumimos que hay uno en una direccion
-				// esta el otro
-				isBidirectional = "bidirectional";
-				
-				//log.info("isBidirectional::"+isBidirectional);
-				
-				if ((1==1)||(isBidirectional != null) && (isBidirectional.equals("bidirectional")))
-				{
-					//((SimpleTEDB)TEDB).getNetworkGraph().addEdge(source, dest, edge);
+				if ((dest!= null)&&(source!=null)){
 					
-					TE_Information tE_infoOtherWay = new TE_Information();
-					tE_infoOtherWay.setNumberWLANs(15);
-					tE_infoOtherWay.initWLANs();
-					IntraDomainEdge edgeOtherWay= new IntraDomainEdge();
+				
+					edge.setSrc_if_id(Long.parseLong((String)jsonObject.get("port_id")));
+					edge.setDst_if_id(Long.parseLong((String)jsonObject.get("peer_port")));
 					
-					edgeOtherWay.setSrc_if_id((Long)jsonObject_src.get("port_no"));
-					edgeOtherWay.setDst_if_id((Long)jsonObject_dst.get("port_no"));
-					edgeOtherWay.setTE_info(tE_infoOtherWay);
+					edge.setType(type);
 					
+					// This is a big problem because info is not initialized from file
+					// and the controller doesn't give information about how many wlans
+					// the are
+					
+					TE_Information tE_info = new TE_Information();
+					System.out.println("Labels: Original ("+labels+") and Parsed ("+labels.substring(labels.indexOf(",")+1,labels.indexOf("]"))+")");
+					tE_info.setNumberWLANs(Integer.parseInt(labels.substring(labels.indexOf(",")+1,labels.indexOf("]"))));
+					tE_info.initWLANs();
+					
+					if (interDomainFile != null)
+					{
+						completeTE_Information(tE_info, source.getRouterID(), dest.getRouterID());
+					}
+					
+					edge.setTE_info(tE_info);
+					
+					
+					edge.setDirectional(true);
+					//Bidirectional case? See other TEDUpdaters.
+				
 					((SimpleTEDB)TEDB).getNetworkGraph().addEdge(source, dest, edge);
-					((SimpleTEDB)TEDB).getNetworkGraph().addEdge(dest, source, edgeOtherWay);
-					
-					completeTE_Information(tE_info, dest.getRouterID(), source.getRouterID());
-					
-					log.info("source::"+source);
-					log.info("dest::"+dest);
-					log.info("edgeOtherWay::"+edgeOtherWay);
-					log.info("edge::"+edge);
-					//log.info("Adding two!");
-				}
-				else
-				{
-					((SimpleTEDB)TEDB).getNetworkGraph().addEdge(source, dest, edge);
-				}
 				
+				} else {
+					System.out.println("Link with an unknown node. Ingnoring...");
+				}
 				//log.info("Edge added:"+edge);
 				//log.info(((SimpleTEDB)TEDB).getIntraDomainLinks().toString());
 			}
@@ -243,6 +212,104 @@ public class TEDUpdaterNOX extends TEDUpdaterController
 			log.info(UtilsFunctions.exceptionToString(e));
 		}
 	}
+
+	
+//	private void parseLinks(String links,Hashtable<String,RouterInfoPM> nodes)
+//	{
+//		try {
+//			//log.info("Inside parseJSON");
+//			JSONParser parser = new JSONParser();
+//			Object obj = parser.parse(links);
+//		
+//			JSONArray msg = (JSONArray) obj;
+//			Iterator<JSONObject> iterator = msg.iterator();
+//			while (iterator.hasNext()) 
+//			{
+//				JSONObject jsonObject = (JSONObject) iterator.next();
+//				//System.out.println(jsonObject.get("src-switch"));
+//				IntraDomainEdge edge= new IntraDomainEdge();
+//				
+//				JSONObject jsonObject_src = (JSONObject) jsonObject.get("src");
+//				JSONObject jsonObject_dst = (JSONObject) jsonObject.get("dst");
+//
+//				RouterInfoPM source = nodes.get(BristoltoFloodlight((String)jsonObject_src.get("dpid")));
+//				RouterInfoPM dest = nodes.get(BristoltoFloodlight((String)jsonObject_dst.get("dpid")));
+//					
+//				//((SimpleTEDB)TEDB).getNetworkGraph().addVertex(source);
+//				//((SimpleTEDB)TEDB).getNetworkGraph().addVertex(dest);
+//
+//				log.info("Adding Vertex->"+source+" hashcode:"+source.hashCode());
+//				log.info("Adding Vertex->"+dest+" hashcode:"+dest.hashCode());
+//				
+//				edge.setSrc_if_id((Long)jsonObject_src.get("port_no"));
+//				edge.setDst_if_id((Long)jsonObject_dst.get("port_no"));
+//				
+//				
+//				// This is a big problem because info is not initialized from file
+//				// and the controller doesn't give information about how many wlans
+//				// the are
+//				
+//				TE_Information tE_info = new TE_Information();
+//				tE_info.setNumberWLANs(15);
+//				tE_info.initWLANs();
+//				
+//				if (interDomainFile != null)
+//				{
+//					completeTE_Information(tE_info, source.getRouterID(), dest.getRouterID());
+//				}
+//				
+//				edge.setTE_info(tE_info);
+//				
+//				String isBidirectional = (String)jsonObject.get("direction");
+//				
+//				
+//				// En Bristol los enlaces son unidirecctionales, pero asumimos que hay uno en una direccion
+//				// esta el otro
+//				isBidirectional = "bidirectional";
+//				
+//				//log.info("isBidirectional::"+isBidirectional);
+//				
+//				if ((1==1)||(isBidirectional != null) && (isBidirectional.equals("bidirectional")))
+//				{
+//					//((SimpleTEDB)TEDB).getNetworkGraph().addEdge(source, dest, edge);
+//					
+//					TE_Information tE_infoOtherWay = new TE_Information();
+//					tE_infoOtherWay.setNumberWLANs(15);
+//					tE_infoOtherWay.initWLANs();
+//					IntraDomainEdge edgeOtherWay= new IntraDomainEdge();
+//					
+//					edgeOtherWay.setSrc_if_id((Long)jsonObject_src.get("port_no"));
+//					edgeOtherWay.setDst_if_id((Long)jsonObject_dst.get("port_no"));
+//					edgeOtherWay.setTE_info(tE_infoOtherWay);
+//					
+//					((SimpleTEDB)TEDB).getNetworkGraph().addEdge(source, dest, edge);
+//					((SimpleTEDB)TEDB).getNetworkGraph().addEdge(dest, source, edgeOtherWay);
+//					
+//					completeTE_Information(tE_info, dest.getRouterID(), source.getRouterID());
+//					
+//					log.info("source::"+source);
+//					log.info("dest::"+dest);
+//					log.info("edgeOtherWay::"+edgeOtherWay);
+//					log.info("edge::"+edge);
+//					//log.info("Adding two!");
+//				}
+//				else
+//				{
+//					((SimpleTEDB)TEDB).getNetworkGraph().addEdge(source, dest, edge);
+//				}
+//				
+//				//log.info("Edge added:"+edge);
+//				//log.info(((SimpleTEDB)TEDB).getIntraDomainLinks().toString());
+//			}
+//			//parseRemainingLinksFromXML(nodes);
+//	 
+//	 
+//		} 
+//		catch (Exception e)
+//		{
+//			log.info(UtilsFunctions.exceptionToString(e));
+//		}
+//	}
 
 	private void completeTE_Information(TE_Information tE_info, String source, String dest) 
 	{
