c222658eeb901a5c18969c00541641637fdcfba9
Ruben Rosales <rrpaton@hotmail.es>
2015-09-24 17:10:15 +0200

    Changes on Save LSPs in Redis

--- src/es/tid/pce/parentPCE/MultiDomainLSPDB.java
+++ src/es/tid/pce/parentPCE/MultiDomainLSPDB.java
@@ -36,5 +36,6 @@ public class MultiDomainLSPDB {
 		}
 		return dbString;
 	}
+	
 
 }
---
+++ src/es/tid/pce/parentPCE/SimpleLSPhop.java
@@ -0,0 +1,13 @@
+package es.tid.pce.parentPCE;
+
+public class SimpleLSPhop {
+	
+	public String routerID=null;
+	
+	public String ifID=null;
+	
+	public String n=null;
+	
+	public String m=null;
+
+}
