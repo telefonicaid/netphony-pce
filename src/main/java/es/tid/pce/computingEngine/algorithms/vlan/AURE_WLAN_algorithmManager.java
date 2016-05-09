83d6db35fd09a8a6ff00d0a4e32f8641453c299c
asgamb <andrea.sgambelluri@gmail.com>
2016-05-09 12:26:19 +0200

    Add files via upload
    modified version supporting delay TE metrics

--- src/main/java/es/tid/pce/computingEngine/algorithms/vlan/AURE_WLAN_algorithmManager.java
+++ src/main/java/es/tid/pce/computingEngine/algorithms/vlan/AURE_WLAN_algorithmManager.java
@@ -6,7 +6,11 @@ import es.tid.pce.computingEngine.algorithms.ComputingAlgorithmManager;
 import es.tid.pce.computingEngine.algorithms.ComputingAlgorithmPreComputation;
 import es.tid.pce.computingEngine.algorithms.multiLayer.OperationsCounter;
 import es.tid.pce.server.wson.ReservationManager;
+import es.tid.tedb.DomainTEDB;
 import es.tid.tedb.TEDB;
+
+import java.net.Inet4Address;
+import java.util.Hashtable;
 
 /**
  * Copy paste from AURE_SSON_algorithmManager
@@ -48,5 +52,10 @@ public class AURE_WLAN_algorithmManager implements ComputingAlgorithmManager {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	@Override
+	public ComputingAlgorithm getComputingAlgorithm(ComputingRequest pathReq,
+													TEDB ted, Hashtable<Inet4Address,DomainTEDB> intraTEDBs) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
